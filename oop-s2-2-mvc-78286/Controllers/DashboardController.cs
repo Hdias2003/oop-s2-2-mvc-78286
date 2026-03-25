deba7397290e0be52ea017f7d4b7ddcb3826db6e
Hdias2003 <hdiasnascimento@gmail.com>
2026-03-25 21:57:59 +0000

    folder structure

---
+++ oop-s2-2-mvc-78286/Controllers/DashboardController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace oop_s2_2_mvc_78286.Controllers
+{
+    public class DashboardController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ oop-s2-2-mvc-78286/Models/Premises.cs
@@ -0,0 +1,6 @@
+﻿namespace oop_s2_2_mvc_78286.Models
+{
+    public class Premises
+    {
+    }
+}
